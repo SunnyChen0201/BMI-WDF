e09fa29b3cba477a3138535db76c4b39a3bdf5b8
m305-28\w401 <sunny1349@gmail.com>
2018-04-26 18:25:20 +0800

    第二次作業

--- WpfApplication2/MainWindow.xaml.cs
+++ WpfApplication2/MainWindow.xaml.cs
@@ -24,5 +24,51 @@ namespace WpfApplication2
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HeightBox.Text = "";
+            HeightBox.Background = Brushes.HotPink;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double h = double.Parse(HeightBox.Text)/100;
+            double kg = double.Parse(kgbox.Text);
+            double bmi = h / (kg * kg);
+
+            
+            Ans.Text = bmi.ToString();
+        }
+
+        private void HeightBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void HeightBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            HeightBox.Text = HeightBar.Value.ToString();
+
+            double h = double.Parse(HeightBox.Text) / 100;
+            double kg = double.Parse(kgbox.Text);
+            double bmi =kg / (h * h);
+
+
+            Ans.Text = bmi.ToString();
+        }
+
+        private void kgBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            kgbox.Text = kgBar.Value.ToString();
+
+            double h = double.Parse(HeightBox.Text) / 100;
+            double kg = double.Parse(kgbox.Text);
+            double bmi = kg / (h * h)  ;
+
+
+            Ans.Text = bmi.ToString();
+
+        }
     }
 }
